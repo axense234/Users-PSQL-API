36b115e2412d3aebed49198abd738e9244c085a8
axense234 <andreicomanescuonline@gmail.com>
2022-12-27 08:59:53 +0200

    Changed host in connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,16 +1,11 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
-const HOST =
-  process.env.NODE_ENV === "production"
-    ? "postgres://ltzfljbs:oqELznM5WxPWQYGjqRqjXUSCzf-EF0eu@mel.db.elephantsql.com/ltzfljbs"
-    : "localhost";
-
 const clientOptions =
   process.env.NODE_ENV === "production"
     ? process.env.PGCSTRING
     : {
-        host: HOST,
+        host: "localhost",
         user: "postgres",
         port: 5432,
         password: process.env.PGPASSWORD,
